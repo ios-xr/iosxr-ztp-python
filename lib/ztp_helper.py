bf93823f168f284c92cfb26bb8ab25f80649cd46
Brandon Him <brandonhim@live.com>
2019-03-27 13:30:37 -0700

    Update file to filename in xrreplace and xrapply

--- lib/ztp_helper.py
+++ lib/ztp_helper.py
@@ -342,7 +342,7 @@ class ZtpHelpers(object):
     def xrapply(self, filename=None, reason=None):
         """Apply Configuration to XR using a file 
           
-           :param file: Filepath for a config file
+           :param filename: Filepath for a config file
                         with the following structure: 
 
                         !
@@ -478,7 +478,7 @@ class ZtpHelpers(object):
     def xrreplace(self, filename=None):
         """Replace XR Configuration using a file 
           
-           :param file: Filepath for a config file
+           :param filename: Filepath for a config file
                         with the following structure: 
 
                         !
